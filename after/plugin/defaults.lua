a2cc32c2b901f02885b9ebcb483230dfe097c17e
Benjamin Moss <drteeth@gmail.com>
2023-04-11 09:44:11 -0400

    stuff

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -24,9 +24,20 @@ vim.keymap.set("n", "<leader>bn", ":bn<CR>")
 vim.keymap.set("n", "<leader>bp", ":bp<CR>")
 vim.keymap.set("n", "<leader>bd", ":bd<CR>")
 
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
 vim.keymap.set('n', '<leader>pf', require('telescope.builtin').find_files, { desc = '[P]roject [F]iles' })
-vim.keymap.set('n', '<leader>/', require('telescope.builtin').live_grep, { desc = 'Fuzzy search' })
-vim.keymap.set('n', '<leader>bb', ":Rg<CR>", { desc = 'Fuzzy search' })
+-- keymap("n", "<leader>pf", ":Files<CR>", opts)
+vim.keymap.set('n', '<leader>pb', ":Buffers<CR>", { desc = '[P]roject [B]uffers' })
+vim.keymap.set('n', '<leader>/', ":Rg<CR>", { desc = 'Fuzzy search' })
+-- vim.keymap.set('n', '<leader>bb', ":Buffers")
 
 vim.cmd("let g:test#strategy = 'neoterm'")
 vim.cmd("let g:test#preserve_screen = 0")
